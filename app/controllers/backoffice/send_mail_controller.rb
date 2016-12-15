96b1b72efed40785fe29eec177cc6ad3edf13bec
Juliane Lima <jujulisan@gmail.com>
2016-12-15 13:00:42 -0200

    Send message with modal

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -10,5 +10,16 @@ class Backoffice::SendMailController < ApplicationController
 
 	def create
 
+		begin
+			AdminMailer.send_message(current_admin, params[:'recipient-name'], params[:'subject-text'], params[:'message-text']).deliver_now
+			@notify_message = I18n.t('messages.email_delivered')
+      @notify_flag = "success"
+    rescue
+      @notify_message = I18n.t('messages.email_not_delivered')
+      @notify_flag = "error"
+    end
+		respond_to do |format|
+			format.js
+		end
 	end
 end
