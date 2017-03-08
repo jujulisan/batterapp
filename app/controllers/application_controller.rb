476fd3f46a3aa94f38fdf3d1fb2fbb1ce8fada36
Juliane Lima <jujulisan@gmail.com>
2017-03-08 14:33:57 -0300

    Configured routes for site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,35 @@
 class ApplicationController < ActionController::Base
+  before_action :store_current_location, :unless => :devise_controller?
 
-  # Pundit
   include Pundit
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # Layouts
   layout :layout_by_resource
-
-  private
-
-  def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
-    redirect_to(request.referrer || root_path)
-  end
 
   protected
 
-  def layout_by_resource
-  	if devise_controller? && resource_name == :admin
-  		'backoffice_devise'
-    elsif devise_controller? && resource_name == :member
-      'site_devise'
-  	else
-  		'application'
-  	end
-  end
+    def layout_by_resource
+    	if devise_controller? && resource_name == :admin
+    		'backoffice_devise'
+      elsif devise_controller? && resource_name == :member
+        'site_devise'
+    	else
+    		'application'
+    	end
+    end
+
+    def user_not_authorized
+      flash[:alert] = I18n.t('messages.not_authorized')
+      redirect_to(request.referrer || root_path)
+    end
+
+    private
+
+    def store_current_location
+      store_location_for(:member, request.url)
+    end
+
 end
