c824bb6b9d83448d6526409a8d8cec8ed3e9f339
Juliane Lima <jujulisan@gmail.com>
2017-04-12 22:01:38 -0300

    Adjusted carousel

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -2,5 +2,6 @@ class Site::HomeController < SiteController
   def index
     @categories = Category.order_by_description
     @ads = Ad.descending_order(params[:page])
+    @carousel = Ad.limit(3).order("RANDOM()")
   end
 end
--- app/helpers/site/home_helper.rb
+++ app/helpers/site/home_helper.rb
@@ -1,2 +1,8 @@
 module Site::HomeHelper
+
+	def first_carousel?(carousel, ad)
+		if carousel.first.id == ad.id
+			"active"
+		end
+	end
 end
